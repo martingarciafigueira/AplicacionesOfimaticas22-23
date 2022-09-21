323b2b67cf1bbe952b61cc662045b9ee565fc138
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2022-09-21 18:01:26 +0200

    Tema 2

---
+++ Tema02 - .Net y 1er programa en Visual Studio/Actividades/EjemploClase1/EjemploClase1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Title = "El plan 2022";
+Console.WriteLine("Bienvenidos al mundo de la programación");
+Console.ForegroundColor = ConsoleColor.Red;
+//Console.Beep(5000,3000);
+Console.WriteLine("Seguro que os va a molar");
+// osdnfñadjfadfovndfiovjvnvn`kasvn`<vje`kvnekvneKOVESIOPAWMNOP
+
+Console.ReadLine();
+
+
---
+++ Tema02 - .Net y 1er programa en Visual Studio/Actividades/EjemploClase2/EjemploClase2/Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Title = "Ventana al cielo";
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+Console.Write("Hola Mundo! ");
+Console.Write("Me llamo Martín ");
+Console.Write("Tengo 32 años");
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+Console.WriteLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Console.WriteLine("Pulse ENTER para cerrar la ventana");
+Console.ReadLine();
