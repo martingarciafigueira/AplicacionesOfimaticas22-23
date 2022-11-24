3ee1b590ec80978407a0557b0107a46e16116686
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2022-11-24 17:43:24 +0100

    Ejemplos

---
+++ Tema06 - Arrays/Ejemplos/Ejemplo2/Ejemplo2/Program.cs
@@ -0,0 +1,27 @@
+﻿//Ejemplo 2: Vamos a crear dos matrices para profesores:
+//nombres: Nombres de profesores
+//apellidos: Apellidos de profesores
+
+//1. Definimos las matrices
+string[] nombres = new string[2] { "Enrique", "Martín" };
+string[] apellidos = new string[2] { "Prieto Magán", "García Figueira" };
+
+//2. Mostramos los nombres
+for (int i = 0; i <= 1; i++)
+{
+    Console.WriteLine(nombres[i]);
+}
+Console.WriteLine();
+//3. Mostramos los apellidos
+for (int i = 0; i <= 1; i++)
+{
+    Console.WriteLine(apellidos[i]);
+}
+Console.WriteLine();
+//4. Mostramos los nombres completos
+for (int i = 0; i <= 1; i++)
+{
+    Console.WriteLine(nombres[i] + " " + apellidos[i]);
+}
+
+Console.ReadLine();
---
+++ Tema06 - Arrays/Ejemplos/Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿//Ejemplo: Guardar los nombres de los alumnos en Meet y mostrarlos por pantalla
+
+//Pasos a seguir
+//1. Crear una matriz con todos los nombres
+string[] nombresMeet = new string[6];
+
+//2. Accedemos a cada una de las posiciones
+nombresMeet[0] = "Alex";
+nombresMeet[1] = "Angel";
+nombresMeet[2] = "Dani";
+nombresMeet[3] = "Jaime";
+nombresMeet[4] = "Roberto";
+nombresMeet[5] = "Sergio";
+
+//3. Pintamos por pantalla todos los nombres
+for (int i = 0; i <= 5; i++)
+{
+    Console.WriteLine(nombresMeet[i]);
+}
+Console.WriteLine();
+
+//4. Cambio Angel por David
+nombresMeet[1] = "David";
+
+//5. Volvemos a pintar con el cambio
+for (int i = 0; i <= 5; i++)
+{
+    Console.WriteLine(nombresMeet[i]);
+}
+
+
+
+Console.ReadLine();
+
+
