6c41f5430d68ff6011941324bd8bbe1305e288ac
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2022-11-24 19:12:33 +0100

    Ejemplos

---
+++ Tema06 - Arrays/Ejemplos/Ejemplo3/Ejemplo3/Program.cs
@@ -0,0 +1,29 @@
+﻿//Ejemplo 3: Crea 3 matrices y guarda la info de los alumnos presenciales
+//Queremos guardar la siguiente información: Nombre, Edad, Videojuego favorito
+//Elige el tipo de dato que necesites
+
+/*
+ * 
+ *  Nombres son: Aaron, Rodrigo, Ruben, Leo, David
+    Edades: 18, 17, 18, 19, 18
+    Videojuegos favs: Candy Crush, WOW, DB, COD, RUST
+ * 
+ * 
+ */
+
+//Ejemplo de lo que debe salir: Aarón, 18, Candy Crush
+
+//Resolución:
+
+//Creamos y rellenamos las 3 matrices
+string[] nombres = new string[] { "Aarón", "Rodrigo", "Rubén", "Leo", "David", "Enrique", "Ramón" };
+int[] edades = new int[] { 18, 17, 18, 19, 18, 29, 31 };
+string[] videojuegos = new string[] { "Candy Crush", "WOW", "DB", "COD", "RUST", "NBA 2K14", "Farming Simulator" };
+
+//Hacemos un bucle y las recorremos
+for (int i = 0; i < nombres.Length; i++)
+{
+    Console.WriteLine(nombres[i] + ", " + edades[i] + ", " + videojuegos[i]);
+}
+
+Console.ReadLine();
---
+++ Tema06 - Arrays/Ejemplos/Ejemplo4/Ejemplo4/Program.cs
@@ -0,0 +1,49 @@
+﻿//Ejemplo 4: Introducimos info para jugadores del Mundial.
+//Queremos saber los siguientes datos:
+// - Cantidad de jugadores (Tamaño de los arrays)
+// - Para cada jugador: (Dentro del bucle)
+//      - Nombres
+//      - Nacionalidades
+//      - Goles
+//      - Asistencias
+
+//Metemos la siguiente información: NOMBRE JUGADOR - NACIONALIDAD - GOLES - ASISTENCIAS
+
+//Ejemplo de como debe aparecer: CRISTIANO - PORTUGUES - 999 - 2
+
+int cantidadJugadores;
+
+//Pedimos el tamaño de los arrays
+Console.Write("Dime cuántos jugadores vas a guardar:");
+cantidadJugadores = int.Parse(Console.ReadLine());
+
+//Creamos los arrays
+string[] nombres = new string[cantidadJugadores];
+string[] nacionalidades = new string[cantidadJugadores];
+int[] goles = new int[cantidadJugadores];
+int[] asistencias = new int[cantidadJugadores];
+
+//Pedimos los datos por pantalla
+for (int i = 0; i < cantidadJugadores; i++)
+{
+    Console.Write("Nombre jugador: ");
+    nombres[i] = Console.ReadLine();
+
+    Console.Write("Nacionalidad jugador: ");
+    nacionalidades[i] = Console.ReadLine();
+
+    Console.Write("Goles jugador: ");
+    goles[i] = int.Parse(Console.ReadLine());
+
+    Console.Write("Asistencias jugador: ");
+    asistencias[i] = int.Parse(Console.ReadLine());
+}
+Console.WriteLine();
+//Mostramos la información por pantalla
+for (int i = 0; i < cantidadJugadores; i++)
+{
+    Console.WriteLine(nombres[i] + " " + nacionalidades[i] + " " + goles[i] + " " + asistencias[i]);
+    Console.WriteLine();
+}
+
+Console.ReadLine();
