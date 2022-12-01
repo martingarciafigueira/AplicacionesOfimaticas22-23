9918a1d40f2d8e848d6eee771c36c75e314ba900
Martin <martin.garcia.figueira@ciclosmontecastelo.com>
2022-12-01 19:05:46 +0100

    Ejercicio8

---
+++ Tema06 - Arrays/Ejercicios/Ejercicio8/Ejercicio8/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int[] valores = new int[100];
+Random aleatorio = new Random();
+
+//Guardamos valores entre 0 y 10 en el array
+for (int i = 0; i < valores.Length; i++)
+{
+    valores[i] = aleatorio.Next(0, 10 + 1);
+}
+
+//Mostramos cada uno de los valores
+foreach (int valor in valores)
+{
+    Console.Write(valor + ", ");
+}
+
+Console.ReadLine();
